d3141bf8870699265b57d36efbec15eb61674431
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-02-11 16:15:55 +0600

    lesson 2 start

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,21 @@ let tasks1 = [
     {id: 3, text: 'React', isDone: false},
 ]
 
+let tasks2 = [
+    {id: 1, text: 'Blood sport', isDone: true},
+    {id: 2, text: 'Spider-Man', isDone: true},
+    {id: 3, text: 'Autumn in New York', isDone: false},
+    {id: 4, text: '2012', isDone: true},
+    {id: 5, text: 'Green mile', isDone: true},
+    {id: 6, text: 'Duck Tales', isDone: false},
+]
+
+
 function App() {
     return (
         <div className="App">
             <TodoList title={'What To Learn?'} task={tasks1}/>
+            <TodoList title={'What To Watch?'} task={tasks2}/>
         </div>
     );
 }
